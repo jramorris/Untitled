296a6cd376164604a537f537f3635ddb5bf1f67d
James Morris <jamesramorris@gmail.com>
2020-09-09 23:33:55 -0700

    face that mouse

---
+++ spooky/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // variables
+    public float movementSpeed = 5f;
+    public GameObject camera;
+    public GameObject bulletSpawnPoint;
+    public float waitTime = 0.0f;
+
+
+    // methods
+    private void Update()
+    {
+        // player facing mouse
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitDist)) {
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+        }
+
+        // move forwards
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot!");
+    }
+}

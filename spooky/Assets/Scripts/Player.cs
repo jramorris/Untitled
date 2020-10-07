88f6881afcd64003638fe4780ad8259a62a05856
James Morris <jamesramorris@gmail.com>
2020-10-06 22:48:05 -0700

    random assets and walking zombie

--- spooky/Assets/Scripts/Player.cs
+++ spooky/Assets/Scripts/Player.cs
@@ -1,18 +1,56 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
     // variables
-    public float movementSpeed = 5f;
     public GameObject camera;
     public GameObject bulletSpawnPoint;
     public float waitTime = 0.0f;
     public GameObject playerObj;
     public GameObject bullet;
     public float points = 0;
+    public float gravity = 20.0F;
+
+    //movement
+    private Rigidbody rb;
+    private CharacterController controller;
+    public float curSpeed = 5f;
+    public float movementSpeed = 5f;
+
+    // joysticks
+    public Joystick moveJoystick;
+    public Joystick lookJoystick;
+
+    // tests
+    private Vector3 moveDirection = Vector3.zero;
+    private bool moveForward = false;
+    private bool moveLeft = false;
+    private bool moveRight = false;
+    private bool moveBack = false;
 
 
     // methods
+
+    private void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+        //rb.freezeRotation = true;
+
+        controller = GetComponent<CharacterController>();
+    }
+
+    void UpdateLookJoystick()
+    {
+        float hoz = lookJoystick.Horizontal;
+        float ver = lookJoystick.Vertical;
+        Vector3 direction = new Vector3(lookJoystick.Horizontal, 0, lookJoystick.Vertical).normalized;
+        Vector3 lookAtPosition = transform.position + direction;
+        transform.LookAt(lookAtPosition);
+
+
+    }
+
     private void Update()
     {
         // player facing mouse
@@ -20,6 +58,7 @@ public class Player : MonoBehaviour
         Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
         float hitDist = 0.0f;
 
+        /* more face mouse
         if (playerPlane.Raycast(ray, out hitDist)) {
             Vector3 targetPoint = ray.GetPoint(hitDist);
             Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
@@ -27,23 +66,52 @@ public class Player : MonoBehaviour
             targetRotation.z = 0;
             playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 7f * Time.deltaTime);
         }
+        */
+
+        Vector3 direction = new Vector3(lookJoystick.Horizontal, 0, lookJoystick.Vertical).normalized;
+        Vector3 lookAtPosition = transform.position + direction;
+        Quaternion targetRotation = Quaternion.LookRotation(lookAtPosition - transform.position);
+        transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 7f * Time.deltaTime);
 
         // move forwards
         if (Input.GetKey(KeyCode.W)) {
-            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            moveForward = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            moveLeft = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            moveRight = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+            moveBack = true;
         }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            moveForward = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            moveLeft = false;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            moveRight = false;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            moveBack = false;
+        }
+
+        // standard assets input manager
+        //Vector3 move = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical"));
+        Vector3 move = new Vector3(moveJoystick.Horizontal, 0, moveJoystick.Vertical).normalized;
+        controller.Move(move * Time.deltaTime * movementSpeed);
+
 
         // shoot
         if (Input.GetMouseButtonDown(0))
